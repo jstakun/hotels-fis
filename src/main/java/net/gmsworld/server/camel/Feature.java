5cce8751d78c4b1a5969af902cf3c769bcfc503f
jstakun <jstakun@redhat>
2017-09-18 14:39:21 +0200

    playing with post

---
+++ src/main/java/net/gmsworld/server/camel/Feature.java
@@ -0,0 +1,16 @@
+package net.gmsworld.server.camel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Feature {
+	
+	public final String type = "Feature";
+	
+	public String _id = null;
+	
+	public Map<String, Object> properties = new HashMap<String, Object>();
+	
+	public Object geometry;
+    
+}
